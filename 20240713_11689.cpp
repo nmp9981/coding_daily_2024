2d206dad05e975bae30c967ec79445fe84c2c93d
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2024-07-14 23:57:20 +0900

    Update 20240713_11689.cpp

--- 20240713_11689.cpp
+++ 20240713_11689.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 typedef long long ll;
 
-ll n, ans;
+ll n, res;
 
 ll Euler(ll n){
-    ans = n;
+    res = n;
     for (ll i = 2; i * i <= n; i++) {
-		if (n % i == 0) ans -= (ans / i);
-		while (n % i == 0) n /= i;
+		if (n % i == 0) res = res-(res/i);
+		while (n % i == 0) n = n/i;
 	}
 
-	if (n > 1) ans -= (ans / n);
-	return ans;
+	if (n > 1) res = res-(res/n);
+	return res;
 }
-
 int main()
 {
 	cin >> n;
