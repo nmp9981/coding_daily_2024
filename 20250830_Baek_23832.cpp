2bdb23d89319a579ccb35282db7d352fb11a1b15
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2025-08-30 10:45:03 +0900

    Create 20250830_Baek_23832.cpp

---
+++ 20250830_Baek_23832.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+//오일러 피함수
+int EulerPI(int n){
+    int res = n;
+    for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) res = res-(res/i);
+		while (n % i == 0) n = n/i;
+	}
+
+	if (n > 1) res = res-(res/n);
+	return res;
+}
+
+//간선 개수 구하기
+void EdgeCount(int x){
+    int cnt = 0;
+    for(int i=2;i<=x;i++){
+        cnt+=EulerPI(i);
+    }
+    cout<<cnt;
+}
+
+//입력
+void Input(){
+    cin>>n;
+}
+
+int main()
+{
+    Input();//입력
+    EdgeCount(n);//간선 개수 구하기
+    return 0;
+}
