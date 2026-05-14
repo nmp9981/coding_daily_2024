9dc2eb7b5fd98451b14749caf3551f6d02343bc4
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2026-05-15 01:10:19 +0900

    Update 20260514_GJK_EPA_Practice.cs

--- 20260514_GJK_EPA_Practice.cs
+++ 20260514_GJK_EPA_Practice.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Practice
@@ -26,12 +27,12 @@ namespace Practice
             // 1만큼 겹침
             var a1 = new List<Point> { new(0, 0), new(2, 0), new(2, 2), new(0, 2) };
             var b1 = new List<Point> { new(1, 0), new(3, 0), new(3, 2), new(1, 2) };
-            Debug.Log($"겹침: {new GJKPractice().GJK_Flow(a1, b1)}");  // true
+            //Debug.Log($"겹침: {new GJKPractice().GJK_Flow(a1, b1)}");  // true
 
             // 떨어짐
             var a2 = new List<Point> { new(0, 0), new(1, 0), new(1, 1), new(0, 1) };
             var b2 = new List<Point> { new(5, 5), new(6, 5), new(6, 6), new(5, 6) };
-            Debug.Log($"분리: {new GJKPractice().GJK_Flow(a2, b2)}");  // false
+            //Debug.Log($"분리: {new GJKPractice().GJK_Flow(a2, b2)}");  // false
 
             // 살짝 겹침 (0.5만큼 위로)
             var a3 = new List<Point> { new(0, 0), new(2, 0), new(2, 2), new(0, 2) };
@@ -41,7 +42,7 @@ namespace Practice
             // 삼각형 vs 사각형
             var a4 = new List<Point> { new(0, 0), new(3, 0), new(1.5f, 3) };
             var b4 = new List<Point> { new(1, 1), new(4, 1), new(4, 4), new(1, 4) };
-            Debug.Log($"비대칭 겹침: {new GJKPractice().GJK_Flow(a4, b4)}");  // true
+            //Debug.Log($"비대칭 겹침: {new GJKPractice().GJK_Flow(a4, b4)}");  // true
         }
 
         public bool GJK_Flow(List<Point> shapeA, List<Point> shapeB)
@@ -69,7 +70,8 @@ namespace Practice
                 if (IsTriangleOrigin(a,p,b))
                 {
                     List<Point> triangleList = new List<Point> { a, p, b };
-                    EPA_Flow(shapeA, shapeB, triangleList);
+                    (float depth, Vector2 direction) = EPA_Flow(shapeA, shapeB, triangleList);
+                    Debug.Log("깊이 : " + depth + " 방향 " + direction.x + " " + direction.y);
                     return true;
                 }
                 else//점 교체 및 방향 변경
@@ -103,9 +105,36 @@ namespace Practice
         /// </summary>
         (float depth, Vector2 direction) EPA_Flow(List<Point> shapeA, List<Point> shapeB, List<Point> triangleList)
         {
-            Edge edge = new Edge();
+            Debug.Log($"EPA 입력 삼각형: {triangleList[0].x},{triangleList[0].y} | {triangleList[1].x},{triangleList[1].y} | {triangleList[2].x},{triangleList[2].y}");
+            //CCW 정렬
+            SortCCW(triangleList);
+            Debug.Log($"EPA 입력 삼각형: {triangleList[0].x},{triangleList[0].y} | {triangleList[1].x},{triangleList[1].y} | {triangleList[2].x},{triangleList[2].y}");
 
-            return (edge.distance, edge.normal);
+            const int MAX_ITER = 32;
+            const float EPSILON = 0.0001f;
+            for(int i = 0; i < MAX_ITER; i++)
+            {
+                //원점에서 가장 가까운 변
+                Edge edge = ClosestEdgeInOrigin(triangleList);
+
+                //해당 방향으로 부터 가장 먼 점
+                Point p = Support(shapeA, shapeB, edge.normal);
+                //점 p를 노말 방향으로 투영한 길이
+                float dist = p.x* edge.normal.x+p.y* edge.normal.y;
+
+                //거리 비교
+                if (Mathf.Abs(edge.distance-dist)<EPSILON)//종료
+                {
+                    return (edge.distance, edge.normal);
+                }
+                else
+                {
+                    triangleList.Insert(edge.index+1, p);
+                }
+            }
+            //최대 반복
+            Edge side = ClosestEdgeInOrigin(triangleList);
+            return (side.distance, side.normal);
         }
 
         /// <summary>
@@ -196,7 +225,53 @@ namespace Practice
         /// <param name="triangleList"></param>
         void SortCCW(List<Point> triangleList)
         {
+            Vector2 p1 = new Vector2(triangleList[1].x-triangleList[0].x, triangleList[1].y - triangleList[0].y);
+            Vector2 p2 = new Vector2(triangleList[2].x - triangleList[0].x, triangleList[2].y - triangleList[0].y);
 
+            float ccwValue = Cross_Vector2(p1,p2);
+            if (ccwValue >= 0)//반시계
+            {
+            }
+            else//시계
+            {
+                (triangleList[1], triangleList[2]) = (triangleList[2], triangleList[1]);
+            }
+        }
+        /// <summary>
+        /// 원점에서 가장 가까운 변 찾기
+        /// </summary>
+        /// <returns></returns>
+        Edge ClosestEdgeInOrigin(List<Point> triangleList)
+        {
+            Edge edge = new Edge();
+            float dist = float.MaxValue;
+
+            int count = triangleList.Count;
+            for(int i = 0; i < count; i++)
+            {
+                //변
+                Point p1 = triangleList[i];
+                Point p2 = triangleList[(i + 1)%count];
+
+                //변을 90도 시계방향으로 회전하면 노말벡터가 나옴
+                //우리는 해당 방향기준으로 원점까지의 거리를 구하면 된다.
+                //방향은 이미 있으니 선분 OA, OB중 아무거나 해당 방향에 투영시킴
+                //원점으로부터 선분 AB까지의 거리를 구할 수 있음
+                Vector2 AB = new Vector2(p2.x-p1.x,p2.y-p1.y);
+                Vector2 ABNormal = new Vector2(AB.y,-AB.x).normalized;
+
+                //노말 방향 투영길이
+                float curDist = ABNormal.x*p1.x+ ABNormal.y*p1.y;
+                if(curDist < dist)//더 가까운 거리
+                {
+                    dist = curDist;
+
+                    edge.distance = dist;
+                    edge.normal = ABNormal;
+                    edge.index = i;
+                }
+            }
+            return edge;
         }
     }
 }
